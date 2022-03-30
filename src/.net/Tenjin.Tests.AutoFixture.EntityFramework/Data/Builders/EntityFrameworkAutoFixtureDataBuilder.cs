2f81001e794beb1522d2474379637f1bc3f4b022
Gerard Gouws <gerard.gouws@capgemini.com>
2022-03-30 21:29:03 +0200

    20220330 - Initial commit of the project.

---
+++ src/.net/Tenjin.Tests.AutoFixture.EntityFramework/Data/Builders/EntityFrameworkAutoFixtureDataBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tenjin.Tests.Data.Builders.AutoFixture;
+using Tenjin.Tests.Data.Builders.AutoFixture.Options;
+
+namespace Tenjin.Tests.AutoFixture.EntityFramework.Data.Builders
+{
+    public abstract class EntityFrameworkAutoFixtureDataBuilder<TDbContext> : AutoFixtureDataBuilder where TDbContext : DbContext
+    {
+        protected EntityFrameworkAutoFixtureDataBuilder(
+            TDbContext dbContext,
+            AutoFixtureDataBuilderOptions? options = null) : base(options)
+        {
+            DbContext = dbContext;
+        }
+
+        public TDbContext DbContext { get; }
+
+        public override void Save()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
