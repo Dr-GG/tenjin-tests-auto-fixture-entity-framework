e5c680580f5b6f30c1315b414d9a94691c4409cf
Gerard Gouws <gerard.gouws@capgemini.com>
2023-06-17 15:32:44 +0200

    Added improvements
    Added Sonar Cloud analysis and support.
    Added code documentation.
    Added easier and re-usable Azure yaml files.

--- src/.net/Tenjin.Tests.AutoFixture.EntityFramework/Data/Builders/EntityFrameworkAutoFixtureDataBuilder.cs
+++ src/.net/Tenjin.Tests.AutoFixture.EntityFramework/Data/Builders/EntityFrameworkAutoFixtureDataBuilder.cs
@@ -4,8 +4,15 @@ using Tenjin.Tests.AutoFixture.Data.Builders.Options;
 
 namespace Tenjin.Tests.AutoFixture.EntityFramework.Data.Builders;
 
+/// <summary>
+/// The extended AutoFixtureDataBuilder that uses a DbContext to build data.
+/// </summary>
+/// <typeparam name="TDbContext"></typeparam>
 public abstract class EntityFrameworkAutoFixtureDataBuilder<TDbContext> : AutoFixtureDataBuilder where TDbContext : DbContext
 {
+    /// <summary>
+    /// Creates a new instance.
+    /// </summary>
     protected EntityFrameworkAutoFixtureDataBuilder(
         TDbContext dbContext,
         AutoFixtureDataBuilderOptions? options = null) : base(options)
@@ -13,8 +20,14 @@ public abstract class EntityFrameworkAutoFixtureDataBuilder<TDbContext> : AutoFi
         DbContext = dbContext;
     }
 
+    /// <summary>
+    /// The DbContext instance being used.
+    /// </summary>
     public TDbContext DbContext { get; }
 
+    /// <summary>
+    /// Saves the current data.
+    /// </summary>
     public override void Save()
     {
         DbContext.SaveChanges();
